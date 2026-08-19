52353bf78c00ee36579fccb7450c3556a304e96e
Gerald Reyes <gerald.oblego.reyes@gmail.com>
2026-08-19 17:42:19 +0800

    Create Reyes.java

---
+++ src/main/java/Activity1/Reyes.java
@@ -0,0 +1,17 @@
+package Activity1;
+
+public class Reyes {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Full Name: Reyes, Gerald O.");
+		System.out.println("Age: 19");
+		System.out.println("Course and Year Level: II-CINS");
+		System.out.println("School: UMAK");
+		System.out.println("Hobbies: Gaming");
+		System.out.println("Favorite Programming Language or Technology: Java");
+		System.out.println("Reason why you want to learn Java: Adapt in the modern technologies that is uprising in the society.");
+
+	}
+
+}
