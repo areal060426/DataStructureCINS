c4a58ef630aec7fbde12326aa8ed5bf159596a2b
Willheime <mj.guillermo.23.mg@gmail.com>
2026-08-19 16:19:05 +0800

    Create Guillermo.java

---
+++ src/main/java/Activity1/Guillermo.java
@@ -0,0 +1,22 @@
+package Activity1;
+
+public class Guillermo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Full Name: Willheime MarieJune E. Guillermo \n");
+		
+		System.out.println("Age: 19 years old \n");
+		
+		System.out.println("Course and Year Level: Bachelor of Science in Information Technology - 2nd Year \n");
+		
+		System.out.println("School: University of Makati \n");
+		
+		System.out.println("Hobbies: Cooking, Playing Games, Reading, and Designing \n");
+		
+		System.out.println("Favorite Programming Language or Technology: Java and Python \n");
+		
+		System.out.println("Reason: I want to study Java because I want to improve my programming skills more and strengthen my problem-solving abilities. \n");
+	}
+
+}
