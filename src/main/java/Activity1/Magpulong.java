77c137909fcecc71ec4548bea42732ec8c7f2e1b
Japhette Louis C. Magpulong <japhettelouis32@gmail.com>
2026-08-19 15:45:59 +0800

    Fixed Code, added println and fixed answers

--- src/main/java/Activity1/Magpulong.java
+++ src/main/java/Activity1/Magpulong.java
@@ -2,14 +2,13 @@ package Activity1;
 
 public class Magpulong {
     public static void main(String[] args) {
-        System.out.print("Full Name: Japhette Louis Magpulong");
-        System.out.print("Age: 19");
-        System.out.print("Course and Year Level: II - CINS");
-        System.out.print("School: University Of Makati");
-        System.out.print("Hobbies: Combat Sports, Playing Video Games, Watching movies");
-        System.out.print("Favorite Programming Language or Technology: C");
-        System.out.print("Reason why you want to learn Java: Because it is a good language to learn the basics of programming\n" +
+        System.out.println("Full Name: Japhette Louis Magpulong");
+        System.out.println("Age: 19");
+        System.out.println("Course and Year Level: BSIT, 2nd year");
+        System.out.println("School: University Of Makati");
+        System.out.println("Hobbies: Combat Sports, Playing Video Games, Watching movies");
+        System.out.println("Favorite Programming Language or Technology: Internet");
+        System.out.println("Reason why you want to learn Java: Because it is a good language to learn the basics of programming" +
                 "and i would imagine has a decent job pull as well when learned");
     }
-
 }
