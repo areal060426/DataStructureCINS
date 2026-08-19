145f66b52db1d0d3356b2ee36d3785d654d2682c
John Carl Rasonable <johncarlrasonable1234@gmail.com>
2026-08-19 17:37:02 +0800

    Update Rasonable.java

--- src/main/java/Activity1/Rasonable.java
+++ src/main/java/Activity1/Rasonable.java
@@ -6,7 +6,7 @@ public class Rasonable {
 		
 	System.out.println("John Carl C. Rasonable");
 	System.out.println("19 Years Old");
-	System.out.println("BSIT 2nd Year Level");
+	System.out.println("Bachelors of Science in Information and Technology, 2nd Year Level");
 	System.out.println("University of Makati");
 	System.out.println("Some of hobbies are Gaming,Watching anime, Workingout");
 	System.out.println("My favorite technology is probably the phone since i can easily use it for communication, take pictures, use social media and i can bring it anywhere i want.");
