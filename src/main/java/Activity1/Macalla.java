adc4f7b3316748740ae60cffa293f91e44b280f5
ajmacalla <alexandra.macalla@umak.edu.ph>
2026-08-19 14:47:22 +0800

    Create Macalla.java
    Completed Activity 1

---
+++ src/main/java/Activity1/Macalla.java
@@ -0,0 +1,19 @@
+package Activity1;
+
+public class Macalla {
+
+	public static void main(String[] args) {
+		System.out.println("Full Name: Alexandra Jane V. Macalla");
+		System.out.println("Age: 19 years old");
+		System.out.println("Course and Year Level: Bachelor of Science in Information Technology - 2nd Year");
+		System.out.println("School: University of Makati");
+		System.out.println("Hobbies: Playing acoustic guitar and bass, " +
+						   "watching movies, writing, and drawing.");
+		System.out.println("Favorite Programming Language or Technology: Java");
+		System.out.println("Reason why you want to learn Java: " + 
+						   "I want to learn Java because I want to challenge myself " +
+						   "to understand how programs work behind the screen " +
+						   "and eventually turn my own ideas into working applications.");
+	}
+
+}
