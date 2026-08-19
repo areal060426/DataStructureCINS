2e4590f10928afe8bd8058bb8f44963430d0d403
ROSEANN JOY SALIENTE <roseann.saliente@umak.edu.ph>
2026-08-19 16:34:15 +0800

    Created Saliente.java

---
+++ src/main/java/Activity1/Saliente.java
@@ -0,0 +1,17 @@
+package Activity1;
+
+public class Saliente {
+
+	public static void main(String[] args) {
+			
+			System.out.println("Full Name: Roseann Joy H. Saliente");
+			System.out.println("Age: 19");
+			System.out.println("Course and Year level: 2nd Year- Information Technology");
+			System.out.println("School: University of Makati");
+			System.out.println("Hobbies: Playing games, Reading Wattpad, Cafe hopping, Drawing blueprints");
+			System.out.println("Favorite Programming Language or Technology: Java");
+			System.out.println("Reason why you want to learn java: Since Java is the first programming language that I learned,\nI want to improve my knowledge about it so I can use it for future jobs.");
+			}
+	}
+
+
