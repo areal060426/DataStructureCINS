81ad6f40929417b4602fbf209f7d080ac9091a32
Sofia Ashley Constantino <sofia.constantino@umak.edu.ph>
2026-08-19 16:58:17 +0800

    Create Constantino.java

---
+++ src/main/java/Activity1/Constantino.java
@@ -0,0 +1,15 @@
+package Activity1;
+
+public class Constantino {
+	public static void main(String[] args) {
+		System.out.println("Full Name: Sofia Ashley T. Constantino");
+		System.out.println("Age: 19 years old");
+		System.out.println("Course and Year Level: BSIT - 2nd Year");
+		System.out.println("School: University of Makati");
+		System.out.println("Hobbies: Watching series and movies, Cooking, and Gaming");
+		System.out.println("Favorite Programming Language: Java Language");
+		System.out.println("The reason why I want to learn Java is because they said that once you learn this language,"
+				+ "\nit will be easier to learn other languages like Python. Also, I think this is the language I am most familiar with,"
+				+ "\nso mastering it would be a strength for me in the future. ");
+	}
+}
