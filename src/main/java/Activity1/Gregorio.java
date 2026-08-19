ab87c48c18f981b203cd79905878bd44e9ef6868
Paul Efhraim Gregorio <pgregorio.9656@umak.edu.ph>
2026-08-19 15:18:47 +0800

    Update Changes
    Created a short description of myself according to the activity

---
+++ src/main/java/Activity1/Gregorio.java
@@ -0,0 +1,39 @@
+package Activity1;
+
+public class Gregorio {
+    public static void main(String[] args) {
+        System.out.println("==========================================================================================");
+        System.out.println("                              STUDENT PROFILE & INFORMATION                               ");
+        System.out.println("==========================================================================================");
+        
+        System.out.print("Full Name:                                ");
+        System.out.println("Gregorio, Paul Efhraim N.");
+        
+        System.out.print("Age:                                      ");
+        System.out.println("19");
+        
+        System.out.print("Course & Year Level:                      ");
+        System.out.println("BSIT major in Network Security, 2nd Year");
+        
+        System.out.print("School:                                   ");
+        System.out.println("University of Makati");
+        
+        System.out.print("Hobbies:                                  ");
+        System.out.println("Photography, Playing the Guitar, Singing, ");
+        System.out.println("                                          Baking, and Fitness/Working Out");
+        
+        System.out.print("Favorite Programming Lang / Technology:   ");
+        System.out.println("Autonomous Flight Systems & Artificial Intelligence");
+        
+        System.out.print("Reason to Learn Java:                     ");
+        System.out.println("To acquire practical programming skills and");
+        System.out.println("                                          build a strong foundation for a future career in technology.");
+        
+        System.out.println("==========================================================================================");
+        
+        System.out.print("Prepared By: ");
+        System.out.println("Gregorio, Paul Efhraim N.");
+        
+        System.out.println("==========================================================================================");
+    }
+}
