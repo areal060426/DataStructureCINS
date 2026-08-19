6c876ea4944c1caf5ed049bcbe581dde7b468e3e
evelasco9881-ship-it <evelasco.9881@umak.edu.ph>
2026-08-19 15:13:36 +0800

    Velasco.java
    Velasco Github + Eclipse Activity

--- src/main/java/Sample.java
+++ src/main/java/Sample.java
@@ -1,9 +1,24 @@
+/*Create a Java program named [YourLastName].java.
+
+Your program should display the following information:
+
+Full Name
+Age
+Course and Year Level
+School
+Hobbies
+Favorite Programming Language or Technology
+Reason why you want to learn Java */
 
 public class Sample {
 
 	public static void main(String[] args) {
-		System.out.print("hello");
-		System.out.print("Test");
+		System.out.print("NAME: EZRA SHADROCK B. VELASCO\n");
+		System.out.print("AGE: 19");
+		System.out.print("COURSE & YEAR LEVEL: BS-IT 2nd YEAR\n");
+		System.out.print("HOBBIES: MUSIC & VIDEO GAMES\n");
+		System.out.print("FAVORITE PROGRAMMING LANGUAGE: JAVA\n");
+		System.out.print("WHY JAVA: I HAVE BACKGROUND KNOWLEDGE AND EXPERIENCE");
 	}
 
 }
