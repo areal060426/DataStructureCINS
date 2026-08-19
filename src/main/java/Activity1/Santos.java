f008e38bc7eba7edce64c4360ea376cb02d1e802
rhinesantos10 <rhinemiguelsantos10@gmail.com>
2026-08-19 18:20:04 +0800

    Santos.java

---
+++ src/main/java/Activity1/Santos.java
@@ -0,0 +1,18 @@
+package Activity1;
+
+
+public class Santos {
+
+	public static void main(String[] args) {
+	System.out.println("Full Name: Santos, Rhine Miguel M.");
+	System.out.println("Age: 19");
+	System.out.println("Course and Year Level: 2nd Year, BSIT");
+	System.out.println("School: University of Makati");
+	System.out.println("Hobbies: Gaming, Cycling, Basketball, and Playing Guitar");
+	System.out.println("Favorite Programming Language: Well...Java is my first programming language as of now, so I'd say Java");
+	System.out.println("Reason whyI want to learn Java: because technology is a huge part of our daily lives now. \nSeeing technology almost everywhere made me think, 'What if I pursue this?' Because of my curiosity, I decided that it was time to start learning about it");	
+		
+		
+	}
+
+}
