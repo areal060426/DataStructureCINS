6a7b76c31f262d85afe6c0d4128778136effb605
Jomel Rosalejos <jomelrosalejos@gmail.com>
2026-08-19 16:10:53 +0800

    added Rosalejos.java

---
+++ src/main/java/Activity1/Rosalejos.java
@@ -0,0 +1,16 @@
+package Activity1;
+
+public class Rosalejos {
+    
+    public static void main(String[] args) {
+        System.out.println("NAME: Jomel Rosalejos");
+        System.out.println("AGE: 19 years old");
+        System.out.println("COURSE AND YEAR LEVEL: BSIT II-CINS");
+        System.out.println("SCHOOL: University of Makati");
+        System.out.println("HOBBIES: Watching anime");
+        System.out.println("FAVORITE PROGRAMMING LANGUAGE OR TECHNOLOGY:\nJava and Python\n");
+        System.out.println("I want to learn Java because it is widely used by big companies, and offers strong job opportunities.");
+        
+    }
+    
+}
