2b1c18be7d53b530e9c31987ebf78ce32014e8ac
Gerald Reyes <050707g.r@gmail.com>
2026-08-19 16:06:01 +0800

    Reyes
    Created Activity

---
+++ src/main/java/Activity1/Reyes.java
@@ -0,0 +1,16 @@
+package Activity1;
+
+public class Reyes {
+
+	public static void main(String[] args) {
+		System.out.println("Full Name: Reyes, Gerald O.");
+		System.out.println("Age: 19");
+		System.out.println("Course and Year Level: BSIT 2nd Year");
+		System.out.println("School: University of Makati");
+		System.out.println("Hobby: Gaming");
+		System.out.println("Favorite Programming Language or Technology: Java");
+		System.out.println("Reason why you want to learn Java: To adapt in the rising of technologies.");
+
+	}
+
+}
