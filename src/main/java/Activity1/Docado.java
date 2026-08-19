ce78473256368cb298b553e8273bdb524919ec5e
Jasper James Docado <scytheruni@gmail.com>
2026-08-19 15:53:00 +0800

    Created Docado.java

---
+++ src/main/java/Activity1/Docado.java
@@ -0,0 +1,15 @@
+package Activity1;
+
+public class Docado {
+
+	public static void main(String[] args) {
+		System.out.println("Name: Jasper James Docado");
+		System.out.println("Age: 18 years old");
+		System.out.println("Course and Year Level: Bachelor of Science in Information Techonology / 2nd year");
+		System.out.println("School: University Of Makati");
+		System.out.println("Hobbies: Gaming, Coding and Listening to music ");
+		System.out.println("Favorite Programming Language or Technology: Mobile Phone");
+		System.out.println("Reason why you want to learn Java: I want to learn Java because I have loved technology since I was a kid, and I believe it is an important part of the future.");
+	}
+
+}
