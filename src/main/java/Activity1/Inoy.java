6f5cced35bdf902b6cc3c4cf0b0ad516af871c36
jeybi-inoy <john.inoy@umak.edu.ph>
2026-08-19 15:48:12 +0800

    Create Inoy.java
    Created and accomplished Activity 1

---
+++ src/main/java/Activity1/Inoy.java
@@ -0,0 +1,26 @@
+package Activity1;
+
+public class Inoy {
+
+	public static void main(String[] args) {
+		System.out.println("Full Name: John Bernard Abdao Inoy\n");
+		
+		System.out.println("Age: 18\n");
+		
+		System.out.println("Course & Year Level: BSIT, 2nd Year\n");
+		
+		System.out.println("School: University of Makati\n");
+		
+		System.out.println("Hobbies: Play games, watch movies, learn dances, create graphic designs, "
+				+ "\nand do karaoke.\n");
+		
+		System.out.println("Favorite Programming Language or Technology: Java\n");
+		
+		System.out.println("The reason why I want to learn more from Java is that it would be a great "
+				+ "\naddition to my skillset, as well as it was our introductory programming "
+				+ "\nlanguage to start with. With that, it would be better for me to master it so "
+				+ "\nthat it would be easier to learn other programming languages that are similar "
+				+ "\nto it.");
+	}
+
+}
