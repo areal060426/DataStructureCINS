0352ce147a2c04adf76dbfd889d925ca1d39f60e
eizethj <eizethmaon19@gmail.com>
2026-08-19 18:52:04 +0800

    Maon

---
+++ src/main/java/Activity1/Maon.java
@@ -0,0 +1,17 @@
+package Activity1;
+
+public class Maon {
+		public static void main(String[] args) {
+			System.out.println("Eizeth Janah Althea A. Maon");
+			System.out.println("20");
+			System.out.println("BS IT - 2nd Year");
+			System.out.println("University of Makati");
+			System.out.println("Watching movies & Cleaning");
+			System.out.println("Java language");
+			System.out.println("I want to learn Java because I want to understand programming better."
+			+ "Sometimes coding can be confusing, so I want to practice more and become more comfortable with it.");
+}
+}
+
+
+
