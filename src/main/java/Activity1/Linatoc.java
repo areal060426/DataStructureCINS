9b27b5ce65b8acc61cacdb2dbffbf18f9acee551
liralinatoc <yatslinatoc@gmail.com>
2026-08-19 22:47:56 +0800

    Update Linatoc.java
    More changes to Linatoc's Activity 1 file

--- src/main/java/Activity1/Linatoc.java
+++ src/main/java/Activity1/Linatoc.java
@@ -3,6 +3,15 @@ package Activity1;
 public class Linatoc {
 
 	public static void main(String[] args) {
+
+		System.out.println("Niurja Lira Pama Linatoc");
+		System.out.println("18");
+		System.out.println("BS Information Technology - Second year");
+		System.out.println("University of Makati");
+		System.out.println("Baking, playing online games, makeup, and sewing");
+		System.out.println("No choice, Java...");
+		System.out.println("It seems pretty versatile and used among many sub-industries of the tech world :)");
+		
 		// TODO Auto-generated method stub
 
 	}
