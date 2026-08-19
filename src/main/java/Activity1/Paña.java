378d405609934b05b2af54bb264f04ec2a028045
Daryl Joshua Paña <dpana.0539@umak.edu.ph>
2026-08-19 18:53:50 +0800

    Create Paña.java

---
+++ src/main/java/Activity1/Paña.java
@@ -0,0 +1,17 @@
+package Activity1;
+
+public class Paña {
+
+	public static void main(String[] args) {
+		System.out.println("Name:Daryl Joshua C. Paña");
+		System.out.println("Age: 19 years old");
+		System.out.println("Course: Bachelor of Science in Information Technology");
+		System.out.println("School: University of Makati");
+		System.out.println("Year level: 2nd year");
+		System.out.println("Hobbies: Watching movies and cooking");
+		System.out.println("Favorite programming: Java");
+		System.out.println("Reason why you want to learn Java: i want to learn java because i want to learn how program or coding works and it will help me to enhance my skills. Also, from what i know it can lead you to many opportunities");
+
+	}
+
+}
