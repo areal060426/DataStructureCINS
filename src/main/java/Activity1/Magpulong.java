a962b49860e0b49c1f0296bab6d5b245aa13d602
Japhette Louis C. Magpulong <japhettelouis32@gmail.com>
2026-08-19 15:26:05 +0800

    Magpulong, Activity 1
    Activity 1: Basic Java Program

---
+++ src/main/java/Activity1/Magpulong.java
@@ -0,0 +1,13 @@
+package Activity1;
+
+public class Magpulong {
+    public static void main(String[] args) {
+        System.out.println("Full Name: Japhette Louis Magpulong");
+        System.out.println("Age: 19");
+        System.out.println("Course and Year Level: II - CINS");
+        System.out.println("School: University Of Makati");
+        System.out.println("Hobbies: Combat Sports, Playing Video Games, Watching Movies");
+        System.out.println("Favorite Programming Language or Technology: Machine Learning");
+        System.out.println("Reason why you want to learn Java: Because it is a good language to learn the basics of programming and I would imagine has a decent job pull as well when learned");
+    }
+}
