4b27a51ea75dda558fee42754bff12d78d636db1
Angelito0126 <angelitobaltar06@gmail.com>
2026-08-19 17:11:58 +0800

    Create baltar.java

---
+++ src/main/java/Activity1/baltar.java
@@ -0,0 +1,15 @@
+package Activity1;
+
+public class baltar {
+
+	public static void main(String[] args) {
+		System.out.print("Angelito Jr. M. Baltar");
+		System.out.println("19 years old");
+		System.out.println("BSIT II-CINS");
+		System.out.println("University of Makati");
+		System.out.println("Gaming, thrifting, running, and going outside");
+		System.out.println("As of now, Java");
+		System.out.println("because it might be the most versitile programming language today");
+	}
+
+}
