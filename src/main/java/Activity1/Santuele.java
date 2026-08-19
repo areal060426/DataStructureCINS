a4d6f78da328752bf0d21dcdd4debd8cab622226
Vhenz Santuele <santuelevhenz@gmail.com>
2026-08-19 15:35:21 +0800

    Create Santuele java file

---
+++ src/main/java/Activity1/Santuele.java
@@ -0,0 +1,16 @@
+package Activity1;
+
+public class Santuele {
+
+	public static void main(String[] args) {
+		System.out.println("Full Name: Vhenz A. Santuele");
+		System.out.println("Age: 22");
+		System.out.println("Course and Year Level: II-CINS");
+		System.out.println("School: UMAK");
+		System.out.println("Hobbies: Creating art, Cooking, and Gaming");
+		System.out.println("Favorite Programming Language or Technology: Java");
+		System.out.println("Reason why you want to learn Java: I want to learn Java because it can help me strengthen my programming and problem-solving skills. \nSince Java is widely used in software development, learning it can also give me a strong foundation for creating applications and understanding object-oriented programming.");
+
+	}
+
+}
