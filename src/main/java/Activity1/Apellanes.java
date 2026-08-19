72e8e242e1f32c74a6abed36ee94aa2442852115
Apellanes <75485460+Apellanes@users.noreply.github.com>
2026-08-19 17:28:17 +0800

    Add files via upload

---
+++ src/main/java/Activity1/Apellanes.java
@@ -0,0 +1,14 @@
+package Activity1;
+
+public class Apellanes {
+
+    public static void main(String[] args) {
+        System.out.println("Full Name: Alesandro Apellanes");
+        System.out.println("Age: 19 Years Old");
+        System.out.println("Course & Year Level: BS Information Technology - 2nd Year");
+        System.out.println("School: University of Makati");
+        System.out.println("Hobbies: Triathlon, Playing Games, Building PCs, and Watching Movies");
+        System.out.println("Favorite Programming Language or Technology: Java");
+        System.out.println("Reason why I want to learn Java: To improve my programming skills and become better at programming.");
+    }
+}
