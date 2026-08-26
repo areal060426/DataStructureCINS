e98f0d7fe281dea27ab34bcc29ddb87d31269872
Kiryll Angelo Caguioa <kiryll.caguioa@umak.edu.ph>
2026-08-26 18:55:24 +0800

    Finish group 1 activity
    to finish the activity for group 1

--- src/main/java/Activity2/Group1.java
+++ src/main/java/Activity2/Group1.java
@@ -5,7 +5,7 @@ public class Group1 {
 
         int[] numbers = {10, 20, 30, 40, 50};
 
-        for (int i = 0; i <= numbers.length; i++) {
+        for (int i = 0; i < numbers.length; i++) {
             System.out.println(numbers[i]);
         }
     }
