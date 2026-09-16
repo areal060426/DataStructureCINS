06a4c695ed5ce443407e7f61dec60591ee49f7b1
Kiryll Angelo Caguioa <kiryll.caguioa@umak.edu.ph>
2026-09-16 22:58:11 +0800

    Update Group1.java
    update

--- src/main/java/Activity4/Group1.java
+++ src/main/java/Activity4/Group1.java
@@ -60,7 +60,21 @@ public class Group1 {
 				System.out.println(names[i] + "-" + studentAve);
 			}
 			
+			for (int i = 0; i < n - 1; i++ ) {
+				for (int j = 0; j < n - 1 - i ; j++) {
 
+					if(grades[j] < grades[j + 1]) {
+						double tempGrade = grades[j];
+						grades[j] = grades[j + 1];
+						grades[j + 1] = tempGrade;
+
+						String tempName = names[j];
+						names[j] = names[j + 1];
+						names[j + 1] = tempName;
+					}
+				}
+			}
+		
 			System.out.println("\n   STUDENT RANKING   ");
 			
 			for (int i = 0; i < n; i++) {
