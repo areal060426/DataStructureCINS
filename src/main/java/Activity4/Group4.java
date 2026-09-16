1cb5023b98f63d39a1317c587a1172894c1fa1d7
JustineLagrada <justinelagrada25@gmail.com>
2026-09-16 17:38:05 +0800

    Update Group4.java

--- src/main/java/Activity4/Group4.java
+++ src/main/java/Activity4/Group4.java
@@ -40,18 +40,99 @@ public class Group4 {
             Scanner sc = new Scanner(System.in);
 
 
-            System.out.println("Number of students: "  );
-            int numberOfStudents = sc.nextInt();
+            System.out.print("Number of students: "  );
+            int numOfStudents = sc.nextInt();
 
-            System.out.println("Number of subjects: "  );
-            int numberOfSubjects = sc.nextInt();
+            System.out.print("Number of subjects: "  );
+            int numOfSubjects = sc.nextInt();
 
-            System.out.println("Name: "  );
-            String name = sc.nextLine();
+  
+            String[] studentNames = new String[numOfStudents];
+            String[] subjectNames = new String[numOfSubjects];
+            double[][] arrOfGrades = new double[numOfStudents][numOfSubjects];
+            double[] studentAve = new double[numOfStudents];
+            
+            sc.nextLine();
+            for (int i = 0; i< numOfSubjects; i++) {
+            
+            	System.out.print("Enter name for subject " + (i + 1) + ": \n" );
+            	subjectNames[i]= sc.nextLine();
+            }
+            
+            
+            for (int i =0; i < numOfStudents; i++) {
+            	System.out.print("Enter Student " + (i + 1) + " Name: ");
+            	studentNames[i] = sc.nextLine();
+            	
+            	
+            	System.out.print("Enter " + studentNames[i] + "'s Grades \n");
+            	for (int j = 0; j < numOfSubjects; j++) {
+            		System.out.println("Enter grade for " + subjectNames[j] + ": ");
+            		arrOfGrades[i][j] = sc.nextDouble();		
+            		studentAve[i] += arrOfGrades[i][j]; 
+            	}
+            	
+            	sc.nextLine();
+            	System.out.println(studentNames[i] + "'s Average: " + (studentAve[i]/numOfSubjects));
 
-            System.out.println("Grades: "  );
-            int grades = sc.nextInt();
-
-            double[][] arrOfGrades = new double[numberOfStudents][numberOfSubjects];
-        }
+            	
+            }
+            
+            double [] subjectAverage = new double[numOfSubjects];
+            double [] highestPerSubject = new double[numOfSubjects];
+            double [] lowestPerSubject = new double[numOfSubjects];
+            
+            for (int j = 0; j < numOfSubjects; j++) {
+            	double sum = 0;
+            	double max = arrOfGrades[0][j];
+            	double min = arrOfGrades[0][j];
+            	
+            for (int i = 0; i < numOfStudents; i++) {
+            	sum += arrOfGrades [i][j];
+            	if (arrOfGrades[i][j] > max) max = arrOfGrades[i][j];
+            	if (arrOfGrades[i][j] < min) min = arrOfGrades[i][j];
+            }
+            
+            subjectAverage[j] = sum / numOfStudents;
+            highestPerSubject[j] = max;
+            lowestPerSubject[j] = min;
+            
+            }
+           
+            System.out.println("\nSUBJECT AVERAGE");
+            for (int j = 0; j < numOfSubjects; j++) {
+           System.out.printf("Subject %s | Average: %.2f\n",
+           subjectNames[j], subjectAverage[j]);
+           
+            }
+     for (int i = 0; i < numOfStudents - 1; i++) {
+    	 for (int j = 0;  j < numOfStudents - 1 - i; j++) {
+    		 if(studentAve[j] > studentAve[j + 1]) {
+    			 double tempAvg = studentAve[j];
+    			 studentAve[j]
+    		 }
+    	 }
+     
+          
+          
+     }
+     
+     System.out.println("\nSUBJECT'S HIGHEST AND LOWEST");
+     for (int j = 0; j < numOfSubjects; j++) {
+     System.out.printf(subjectNames[j] + "'s Highest: %.2f | Lowest: %.2f\n",
+    	 highestPerSubject[j], lowestPerSubject[j]);    
+     }
+     
+     System.out.println("\n\t STUDENT RANKING");
+     
+        
+   }
 }
+          
+            
+            
+            
+        
+        
+        
+
