95a41020eb8c71de88bcbf58eb76b3e0c03756e7
Angelica-Estores <aestores.9798@umak.edu.ph>
2026-09-16 11:53:40 +0800

    Update Group4.java

--- src/main/java/Activity4/Group4.java
+++ src/main/java/Activity4/Group4.java
@@ -34,6 +34,24 @@ Determine lowest grade per subject.
 Display students who failed any subject.
 
 */
+import java.util.Scanner;
 public class Group4 {
+        public static void main(String[] args){
+            Scanner sc = new Scanner(System.in);
 
+
+            System.out.println("Number of students: "  );
+            int numberOfStudents = sc.nextInt();
+
+            System.out.println("Number of subjects: "  );
+            int numberOfSubjects = sc.nextInt();
+
+            System.out.println("Name: "  );
+            String name = sc.nextLine();
+
+            System.out.println("Grades: "  );
+            int grades = sc.nextInt();
+
+            double[][] arrOfGrades = new double[numberOfStudents][numberOfSubjects];
+        }
 }
